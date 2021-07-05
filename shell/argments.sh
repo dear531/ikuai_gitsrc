bc723a5f99e4a95cbc6cd5d0b267914f13ca0a57
Leo Cheung <lyzhang@ikuai8.com>
2021-07-05 18:22:21 +0800

    test $@ and $*

---
+++ shell/argments.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+for i in "${@}"; do
+	echo "$i";
+done
+
+for i in "a${*}b"; do
+	echo "111$i";
+done
+	echo "222"
+
+for ((i = 0; i <= $#; i++)); do
+	eval echo \$${i}
+done
+
+echo "IFS:[$IFS]"
+
+echo "$*"
+echo $@e
---
+++ shell/proccess_id.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+echo "$$"
+
+./proccess_id_sub.sh
