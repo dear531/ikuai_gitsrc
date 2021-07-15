3eb2c9e7425c60e3f810d545ab6ce0bb9ea44955
Leo Cheung <lyzhang@ikuai8.com>
2021-07-15 15:35:09 +0800

    abc return code show

--- shell/exit_status.sh
+++ shell/exit_status.sh
@@ -2,9 +2,12 @@
 
 ## leocheung@leocheung-CW65S:~/source/ikuai_gitsrc/shell$ bash exit_status.sh 
 ## 0
-## 1
+## return_status:1
+## exit_status.sh: 行 21: abc: 未找到命令
+## abc:127
 ## leocheung@leocheung-CW65S:~/source/ikuai_gitsrc/shell$ echo $?
 ## 1
+
 
 
 return_status()
@@ -16,7 +19,10 @@ ls > /dev/null 2>&1
 echo $?
 
 return_status;
-echo $?
+echo "return_status:$?"
+
+abc
+echo "abc:$?"
 
 return_status;
 exit
