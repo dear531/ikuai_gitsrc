f5448f8822791afc6dd0258ff5d087b7e2303c9f
Leo Cheung <lyzhang@ikuai8.com>
2021-07-16 11:24:31 +0800

    usage of alias

---
+++ shell/alias.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+alias ls='ls -lrt'
+ls
+
+## leocheung@leocheung-CW65S:~/source/ikuai_gitsrc/shell$ alias ls='ls -lrt'
+## leocheung@leocheung-CW65S:~/source/ikuai_gitsrc/shell$ ls
+## 总用量 72
+## -rwxr-xr-x 1 leocheung root   28 3月  10 16:37 1.sh
+## -rwxr-xr-x 1 leocheung root  474 3月  10 17:11 virable1.sh
+## -rw-r--r-- 1 leocheung root    0 3月  10 18:48 config.db
+## -rw-r--r-- 1 leocheung root 2048 3月  10 18:55 config2.db
+## -rwxr-xr-x 1 leocheung root  189 3月  11 14:38 2.sh
+## -rwxr-xr-x 1 leocheung root  152 3月  11 15:06 convert_lower.sh
+## -rwxr-xr-x 1 leocheung root   29 7月   2 17:30 20210702.first.bash.sh
+## -rw-r--r-- 1 leocheung root  201 7月   5 16:36 argments.sh
+## -rw-r--r-- 1 leocheung root   46 7月   5 16:48 proccess_id.sh
+## -rwxr-xr-x 1 leocheung root   34 7月   5 16:50 proccess_id_sub.sh
+## -rw-r--r-- 1 leocheung root  262 7月   5 18:51 shift.sh
+## -rw-r--r-- 1 leocheung root 1202 7月   9 16:58 getopts.sh
+## -rw-r--r-- 1 leocheung root  254 7月   9 18:37 test.snmptrap.sh
+## -rw-r--r-- 1 leocheung root   71 7月   9 18:38 here_docment.sh
+## -rwxr-xr-x 1 leocheung root  118 7月  14 15:11 cat.sh
+## -rw-r--r-- 1 leocheung root  390 7月  15 15:34 exit_status.sh
+## -rw-r--r-- 1 leocheung root   23 7月  15 15:47 signal.sh
+## -rw-r--r-- 1 leocheung root   45 7月  15 17:15 envirament.sh
+## -rw-r--r-- 1 leocheung root   36 7月  16 11:22 alias.sh
+
