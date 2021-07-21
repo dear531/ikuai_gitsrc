fee8dce84a588971bc22e53e163256bb235bedc2
Leo Cheung <lyzhang@ikuai8.com>
2021-07-21 12:28:49 +0800

    test or [ for file and string test

--- shell/op.sh
+++ shell/op.sh
@@ -11,3 +11,27 @@ echo "[ 1 -le 2 ]:$?"
 
 [ 1 -ge 0 ]
 echo "[ 1 -ge 0 ]:$?"
+
+a=1
+[ "$a" -eq 1 ]
+echo "a=1; [ \"\$a\" -eq 1 ]:$?"
+
+b=1
+[ "$a" -eq "$b" ]
+echo "a=1;b=1; [ \"\$a\" -eq \"\$b\" ] : $?"
+
+[ -a op.sh ]
+echo "[ -a op.sh ]:$?"
+
+[ -b /dev/sda ]
+echo "[ -b /dev/sda ] :$?"
+
+[ -c /dev/tty ]
+echo "[ -c /dev/tty ] :$?"
+
+[ -r $0 ]
+echo "[ -r \$0 ]:$?"
+
+## arg1file newer than arg2file
+[ $0 -nt /dev/tty ]
+echo "[ $0 -nt /dev/tty ]:$?"
