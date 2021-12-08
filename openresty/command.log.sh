3c987a4c55b1c1065147b1cf3c23d26d3a5f9054
Leo Cheung <lyzhang@ikuai8.com>
2021-12-08 17:26:55 +0800

    http for baidu.com

--- openresty/command.log.sh
+++ openresty/command.log.sh
@@ -388,3 +388,48 @@ Connection: keep-alive
 
 test.com
 leocheung@leocheung-CW65S:~/source/ikuai_gitsrc/openresty$ 
+
+==========api test==========
+resty -e 'local sock = ngx.socket.tcp()
+        sock:settimeout(1000)  -- one second timeout
+        local ok, err = sock:connect("www.baidu.com", 80)
+        if not ok then
+            ngx.say("failed to connect: ", err)
+            return
+        end
+        local req_data = "GET / HTTP/1.1\r\nHost: www.baidu.com\r\n\r\n"
+        local bytes, err = sock:send(req_data)
+        if err then
+            ngx.say("failed to send: ", err)
+            return
+        end
+        local data, err, partial = sock:receive()
+        if err then
+            ngx.say("failed to receive: ", err)
+            return
+        end
+        sock:close()
+        ngx.say("response is: ", data)'
+
+resty -e 'local sock = ngx.socket.tcp()
+>         sock:settimeout(1000)  -- one second timeout
+>         local ok, err = sock:connect("www.baidu.com", 80)
+>         if not ok then
+>             ngx.say("failed to connect: ", err)
+>             return
+>         end
+>         local req_data = "GET / HTTP/1.1\r\nHost: www.baidu.com\r\n\r\n"
+>         local bytes, err = sock:send(req_data)
+>         if err then
+>             ngx.say("failed to send: ", err)
+>             return
+>         end
+>         local data, err, partial = sock:receive()
+>         if err then
+>             ngx.say("failed to receive: ", err)
+>             return
+>         end
+>         sock:close()
+>         ngx.say("response is: ", data)'
+response is: HTTP/1.1 200 OK
+
